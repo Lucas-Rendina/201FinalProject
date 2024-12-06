165f621266b7c6dea2d839228541b51362f48b48
Felix <felixzc8@gmail.com>
2024-12-05 21:40:51 -0800

    dbconnection setup

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -1,11 +1,29 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-	private static final String URL = "";
-    private static final String USER = "";
-    private static final String PASSWORD = "";
+    private static String URL;
+    private static String USER;
+    private static String PASSWORD;
+
+    static {
+        // Load properties when the class is first loaded
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream("db.properties")) {
+            props.load(input);
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the error: for example, rethrow as RuntimeException or handle gracefully
+        }
+    }
 
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
