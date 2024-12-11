58e80b3b5e2fa98dd6f5545658fbd41531fd16c8
Benjamin Lu <85896891+blu3542@users.noreply.github.com>
2024-12-11 15:26:42 -0800

    Update calendar.js

--- src/main/webapp/js/calendar.js
+++ src/main/webapp/js/calendar.js
@@ -56,6 +56,8 @@ function handleTokenResponse(response) {
     }
     console.log("Access token received:", response.access_token);
     content.style.display = "block";
+	authorizeButton.style.display = "none"; 
+
     listUpcomingEvents();
 }
 
